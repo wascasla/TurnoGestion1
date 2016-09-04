cd89997ff10ef6acda18f883ed681a4d2eca4c99
was <wascasla@hotmail.com>
2016-09-03 21:26:41 -0300

    agregue nuevo buscador de pacientes

--- languages/es.py
+++ languages/es.py
@@ -179,6 +179,7 @@
 'Enter an integer between %(min)g and %(max)g': 'Enter an integer between %(min)g and %(max)g',
 'enter an integer between %(min)g and %(max)g': 'introduzca un entero entre %(min)g y %(max)g',
 'enter date and time as %(format)s': 'introduzca fecha y hora como %(format)s',
+'Enter date and time as %(format)s': 'Enter date and time as %(format)s',
 'Enter from %(min)g to %(max)g characters': 'Enter from %(min)g to %(max)g characters',
 'Error logs for "%(app)s"': 'Bitácora de errores en "%(app)s"',
 'errors': 'errores',
